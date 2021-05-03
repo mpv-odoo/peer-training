48e10d5c9a236660bba6c3a3c3135de11fb3d7e6
Martin Petrov <mpv@odoo.com>
2021-05-03 12:05:28 -0700

    added ny-shoes module skeleton

---
+++ ny-shoes/__manifest__.py
@@ -0,0 +1,22 @@
+{
+   'name': 'NY Shoes',
+   'summary':"""Need to auto calculate price for NY P&W Shoes""",
+   'description':"""
+      NY P&W Shoes is a shoe distributor in New York.
+      They buy shoes in large quantities from China 
+      and redistribute them here in the US. They 
+      negotiate prices per pair but they sell by
+      case and buy by the case. 
+   """,
+   'author': 'Martin Petrov',
+   'website': '',
+   'category': 'Training',
+   'version': '.1',
+   'depends': ['base'],
+   'date': [
+
+   ],
+   'demo': [
+
+   ]
+}
